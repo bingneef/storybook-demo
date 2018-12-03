c69682352cb1b38fe4a739e12f56e9639920f803
Bing Steup <bingsteup@gmail.com>
2018-12-03 23:28:10 +0100

    Minor test fix

--- src/__tests__/snapshots/index.js
+++ src/__tests__/snapshots/index.js
@@ -2,7 +2,7 @@ import initStoryshots from '@storybook/addon-storyshots';
 import { imageSnapshot } from '@storybook/addon-storyshots-puppeteer';
 import devices from 'puppeteer/DeviceDescriptors';
 
-const storybookUrl = true ? 'http://localhost:9009' : `file://${__dirname}/../../storybook-static`;
+const storybookUrl = true ? 'https://storybook-demo-cb4d0h6zh.now.sh/storybook-static/index.html' : `file://${__dirname}/../../storybook-static`;
 
 const getScreenshotOptions = ({ _context, _url }) => ({
   fullPage: false,
