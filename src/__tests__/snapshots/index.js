3dfc3930224d3f99892a2f87cc0ac4621dbc0a97
Bing Steup <bingsteup@gmail.com>
2018-12-05 11:29:27 +0100

    Fix tests

--- src/__tests__/snapshots/index.js
+++ src/__tests__/snapshots/index.js
@@ -2,18 +2,32 @@ import initStoryshots from '@storybook/addon-storyshots';
 import { imageSnapshot } from '@storybook/addon-storyshots-puppeteer';
 import devices from 'puppeteer/DeviceDescriptors';
 
-const storybookUrl = true ? 'http://localhost:9009' : `file://${__dirname}/../../storybook-static`;
+const storybookUrl = process.env.STORYBOOK_URL || 'http://localhost:9009';
 
 const getScreenshotOptions = ({ _context, _url }) => ({
   fullPage: false,
 })
 
 const customizePage = page => {
-  return page.emulate(devices['iPhone 6'])
+  return Promise.all([
+    // page.emulate(devices['iPhone 6']),
+    page.addStyleTag({ content: `* { transition-property: none !important; transform: none !important; animation: none !important;` }),
+  ]);
 }
 
-const beforeScreenshot = page => {
-  return page.addStyleTag({ content: `* { transition-property: none !important; transform: none !important; animation: none !important;` })
-}
+const getMatchOptions = ({ context: { kind, story }, url }) => {
+  return {
+    failureThreshold: 0,
+    failureThresholdType: 'percent',
+  };
+};
 
-initStoryshots({ suite: 'Image storyshots', test: imageSnapshot({ storybookUrl, getScreenshotOptions, customizePage, beforeScreenshot }) });
+const beforeScreenshot = (page, { context: { kind, story }, url }) => {
+  return new Promise(resolve =>
+    setTimeout(() => {
+      resolve();
+    }, 1500)
+  );
+};
+
+initStoryshots({ suite: 'Image storyshots', test: imageSnapshot({ storybookUrl, getScreenshotOptions, beforeScreenshot, customizePage, getMatchOptions }) });
