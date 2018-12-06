cf2a40c15d2b6b6ca0eabde62d65513ce6a921da
Bing Steup <bingsteup@gmail.com>
2018-12-06 15:22:48 +0100

    Add desktop + iPhone screens

--- src/__tests__/snapshots/index.js
+++ src/__tests__/snapshots/index.js
@@ -8,26 +8,28 @@ const getScreenshotOptions = ({ _context, _url }) => ({
   fullPage: false,
 })
 
-const customizePage = page => {
-  return Promise.all([
-    page.emulate(devices['iPhone 6']),
-    page.addStyleTag({ content: `* { transition-property: none !important; transform: none !important; animation: none !important;` }),
-  ]);
-}
-
-const getMatchOptions = ({ context: { kind, story }, url }) => {
-  return {
-    failureThreshold: 0,
-    failureThresholdType: 'percent',
+for (let device of [null, 'iPhone 6']) {
+  const customizePage = page => {
+    return Promise.all([
+      device && page.emulate(devices[device]),
+      page.addStyleTag({ content: `* { transition-property: none !important; transform: none !important; animation: none !important;` }),
+    ]);
+  }
+  
+  const getMatchOptions = ({ context: { kind, story }, url }) => {
+    return {
+      failureThreshold: 0,
+      failureThresholdType: 'percent',
+    };
   };
-};
-
-const beforeScreenshot = (page, { context: { kind, story }, url }) => {
-  return new Promise(resolve =>
-    setTimeout(() => {
-      resolve();
-    }, 1500)
-  );
-};
-
-initStoryshots({ suite: 'Image storyshots', test: imageSnapshot({ storybookUrl, getScreenshotOptions, beforeScreenshot, customizePage, getMatchOptions }) });
+  
+  const beforeScreenshot = (page, { context: { kind, story }, url }) => {
+    return new Promise(resolve =>
+      setTimeout(() => {
+        resolve();
+      }, 1500)
+    );
+  };
+  
+  initStoryshots({ suite: 'Image storyshots', test: imageSnapshot({ storybookUrl, getScreenshotOptions, beforeScreenshot, customizePage, getMatchOptions }) });
+}
